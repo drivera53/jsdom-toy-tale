7b4d193a19a956f67e041d461114700f007b1fc3
Danny Rivera <samboard@hotmail.com>
2021-05-14 14:06:06 -0400

    commit tree 8f76693d08ea1e1b96fe269d0d92d9c8bd663aa3

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  // Add listener to 'Add Toy' button to show or hide form
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
+    // Hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -13,3 +14,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Fetch Andy's Toys
+function getToys() {
+  return fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+}
